86bbba8f9f242d5edc5e0a3ea7013d5f1cd34d75
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2019-10-08 23:03:33 -0700

    Update toolchains again, add more toolchain tests. Will finish tomorrow.

--- cmake_scripts/toolchains/i386-general-toolchain.cmake
+++ cmake_scripts/toolchains/i386-general-toolchain.cmake
@@ -1,9 +1,15 @@
+#
+# i386 toolchain for a random 386 environment
+#
+# Jeremy Lorelli, Oct. 2019
+#
 set(CMAKE_CROSSCOMPILING ON)
 
-set(CMAKE_SYSTEM_NAME "none")
+set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR "i386")
 set(CMAKE_EXECUTALE_FORMAT "ELF")
 
+set(CMAKE_ASM_COMPILER	"clang")
 set(CMAKE_C_COMPILER 	"clang")
 set(CMAKE_CXX_COMPILER 	"clang++")
 set(CMAKE_LINKER 		"llvm-link")
@@ -14,6 +20,7 @@ set(CMAKE_NM 			"llvm-nm")
 set(CMAKE_RANLIB 		"llvm-ranlib")
 set(CMAKE_AR 			"llvm-ar")
 
+set(CMAKE_ASM_FLAGS 			"${CMAKE_ASM_FLAGS} -m32")
 set(CMAKE_C_FLAGS		"${CMAKE_C_FLAGS} -D_x86_ -D_i386_ -target i386-pc-none-gnu -std=c99" CACHE "C Flags")
 set(CMAKE_CXX_FLAGS		"${CMAKE_CXX_FLAGS} -D_x86_ -D_i386_ -target i386-pc-none-gnu -std=c++17" CACHE "C++ Flags")
 set(CMAKE_EXE_LINKER_FLAGS 		"${CMAKE_EXE_LINKER_FLAGS} -target i386-pc-none-gnu" CACHE INTERNAL "EXE Linker Flags")
--- run-tests.sh
+++ run-tests.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
+echo -e "\n====================\nAVR GENERIC Cross Compile\n====================\n"
 mkdir build 
-cd build && cmake ../ -DCMAKE_TOOLCHAIN_FILE=cmake_scripts/toolchains/avr-toolchain.cmake -DAVR_TEST=1 -DMCU=atmega644pa
+cd build && cmake ../ -DCMAKE_TOOLCHAIN_FILE=cmake_scripts/toolchains/avr-toolchain.cmake -DAVR_TEST=1 -DMCU=atmega644pa -Wno-dev
 make
 cd .. && rm -rf build
 
+echo -e "\n====================\ni386 LINUX Cross Compile\n====================\n"
 mkdir build
-cd build && cmake ../ -DCMAKE_TOOLCHAIN_FILE=cmake_scripts/toolchains/i386-linux-toolchain.cmake -DI386_LINUX_TEST=1 
+cd build && cmake ../ -DCMAKE_TOOLCHAIN_FILE=cmake_scripts/toolchains/i386-linux-toolchain.cmake -DI386_LINUX_TEST=1 -Wno-dev
 make
 cd .. && rm -rf build 
 
+echo -e "\n====================\ni386 GENERIC Cross Compile\n====================\n"
 mkdir build
-cd build && cmake ../ -DCMAKE_TOOLCHAIN_FILE=cmake_scripts/toolchains/i386-toolchain.cmake -DI386_GENERIC_TEST=1 
+cd build && cmake ../ -DCMAKE_TOOLCHAIN_FILE=cmake_scripts/toolchains/i386-toolchain.cmake -DI386_GENERIC_TEST=1 -Wno-dev
 make
 cd .. && rm -rf build 
 
+echo -e "\n====================\ni686 MINGW Cross Compile\n====================\n"
 mkdir build
-cd build && cmake ../ -DCMAKE_TOOLCHAIN_FILE=cmake_scripts/toolchains/i686-mingw-toolchain.cmake -DI686_MINGW_TEST=1 
+cd build && cmake ../ -DCMAKE_TOOLCHAIN_FILE=cmake_scripts/toolchains/i686-mingw-toolchain.cmake -DI686_MINGW_TEST=1 -Wno-dev
+make
+cd .. && rm -rf build 
+
+echo -e "\n====================\nZILOG Z80 Cross Compile\n====================\n"
+mkdir build
+cd build && cmake ../ -DCMAKE_TOOLCHAIN_FILE=cmake_scripts/toolchains/z80-toolchain.cmake -DZ80_TEST=1 -Wno-dev -DRAM_START=32768 -DRAM_SIZE=32767 -DCODE_START=1 -DCODE_SIZE=32767 -DDATA_START=35000
 make
 cd .. && rm -rf build 
